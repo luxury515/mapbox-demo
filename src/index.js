30425ffe23ceaf8f13512a9fd8641e9b57785515
chae <luxury515@naver.com>
2022-08-08 14:55:30 +0900

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BaseMap from "components/map/basemap/BaseMap";
 import SymbolMap from "components/map/symbolmap/SymbolMap";
 import ToolTipMap from "components/map/tooltip/ToolTipMap";
-import GeoCoderMap from "components/map/geocodermap/GeoCoderMap";
+import MarkerDefault from "components/map/markermap/MarkerDefault";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -19,7 +19,7 @@ root.render(
         <Route path="1" element={<BaseMap />} />
         <Route path="2" element={<ToolTipMap />} />
         <Route path="3" element={<SymbolMap />} />
-        <Route path="4" element={<GeoCoderMap />} />
+        <Route path="4" element={<MarkerDefault />} />
       </Route>
     </Routes>
   </BrowserRouter>
