219345beed3a0adc7121324879538715dd9c23c2
chae <luxury515@naver.com>
2023-01-18 10:40:55 +0900

    add index.js

--- src/index.js
+++ src/index.js
@@ -11,9 +11,6 @@ import MarkerDefault from 'components/map/markermap/MarkerDefault';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    // <React.StrictMode>
-    //   <App />
-    // </React.StrictMode>
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<App />}>
